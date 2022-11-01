c9ae9414833b93cd43ff83746ec7a1bb8516a02d
Harrison Truscott <miner.harry567@gmail.com>
2022-11-01 12:48:19 -0400

    scheduling test

---
+++ ray_scheduling_test.py
@@ -0,0 +1,34 @@
+import time
+import ray
+from ray.util.multiprocessing import Pool
+from ray.util.scheduling_strategies import NodeAffinitySchedulingStrategy
+
+
+ray.init();
+print("waiting for display node...");
+most_display = None;
+while most_display is None:
+    nodes = ray.nodes();
+    max_display = 0;
+    for n in nodes:
+        if "Display" in n["Resources"]:
+            d = n["Resources"]["Display"];
+            if d > max_display:
+                most_display = n["NodeID"];
+                max_display = d
+    time.sleep(5);
+
+@ray.remote(scheduling_strategy=NodeAffinitySchedulingStrategy(most_display,soft=True))
+def show_context():
+    context = ray.get_runtime_context();
+    c = context.get()
+    c["resources"] = context.get_assigned_resources();
+    print(c);
+    return c;
+
+
+refs = [show_context.remote() for _ in range(20)];
+print(ray.get(refs));
+
+    
+
