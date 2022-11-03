7c41e1f85582f0990ac77af1ca917df581f3a775
minerharry <35383543+minerharry@users.noreply.github.com>
2022-11-03 01:15:03 -0400

    hm

--- play_level.py
+++ play_level.py
@@ -591,7 +591,7 @@ if __name__== "__main__":
     runConfig.view_distance = 3.75;
     runConfig.task_obstruction_score = task_obstruction_score;
     runConfig.external_render = False;
-    runConfig.parallel_processes = 6;
+    runConfig.parallel_processes = len(total_bundles);
     runConfig.chunkFactor = 24;
     runConfig.saveFitness = False;
 
