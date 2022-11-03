b26bc0c1bb244570cee3c3ff50b6318aac86abd4
minerharry <35383543+minerharry@users.noreply.github.com>
2022-11-02 22:26:33 -0400

    s

--- ray_scheduling_test.py
+++ ray_scheduling_test.py
@@ -21,18 +21,20 @@ while most_display is None:
 print("display node obtained:",most_display);
 
 @ray.remote(scheduling_strategy=NodeAffinitySchedulingStrategy(most_display,soft=True))
-def show_context():
-    context = ray.get_runtime_context();
-    c = context.get()
-    c["resources"] = context.get_assigned_resources();
-    print(c);
-    time.sleep(4);
-    return c;
+class contextActor:
+    def show_context(self):
+        context = ray.get_runtime_context();
+        c = context.get()
+        c["resources"] = context.get_assigned_resources();
+        print(c);
+        time.sleep(4);
+        return c;
 
 print("Cluster total resources:",ray.cluster_resources());
 print("Cluster resource availability:",ray.available_resources());
 print("Cluster nodes:",ray.nodes());
-refs = [show_context.remote() for _ in range(20)];
+refs = [contextActor.remote() for _ in range(20)];
+[c.show_context.remote() for c in refs];
 ray.get(refs);
 
     
