e5d6be9ae382d672e3d0bd2efe86f5ae61baecb6
Harrison Truscott <miner.harry567@gmail.com>
2023-01-29 19:52:02 -0500

    fix type checking stuff

--- id_data.py
+++ id_data.py
@@ -1,8 +1,13 @@
-from typing import Generic, NamedTuple, TypeVar
+from typing import Generic, NamedTuple, TypeVar, TYPE_CHECKING
 
 K = TypeVar("K")
-class _IdData(NamedTuple):
-    id:int
-    data:K
-class IdData(_IdData,Generic[K]):
-    pass;
+if TYPE_CHECKING:
+    class IdData(NamedTuple,Generic[K]):
+        id:int
+        data:K
+else:
+    class _IdData(NamedTuple):
+        id:int
+        data:K
+    class IdData(_IdData,Generic[K]):
+        pass;
